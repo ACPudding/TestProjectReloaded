fa1ed42cc48ae6080a206f3b3ba265329bda69a5
ACPudding <miaomiao2130@gmail.com>
2020-08-05 01:37:06 +0800

    调整UI.

--- FGOSBIAReloaded/Properties/CommonStrings.cs
+++ FGOSBIAReloaded/Properties/CommonStrings.cs
@@ -8,7 +8,7 @@ namespace FGOSBIAReloaded.Properties
 {
     class CommonStrings
     {
-        public static string Version = "版本号:  v2.4.1";
-        public static string VersionTag = "v2.4.1";
+        public static string Version = "版本号:  v2.4.2";
+        public static string VersionTag = "v2.4.2";
     }
 }
